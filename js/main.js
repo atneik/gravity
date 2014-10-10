322f27bf23c1a43fe2e89d598d34a9e31387799d
Aniket Handa <atneik@gmail.com>
2014-10-10 00:52:30 -0700

    audio loading

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var jsonDataURL = "https://dl.dropboxusercontent.com/u/23289062/gravity-project-
 
 var populateLinksToPlayerID = function(links, player, kind){
   for(var type in links){
-    console.log( type );
+    //console.log( type );
     var newSrcChild = document.createElement('source');
     newSrcChild.setAttribute('type', kind + '/' + type);
     newSrcChild.setAttribute('src', links[type]);
@@ -30,13 +30,13 @@ var populateLinksToPlayerID = function(links, player, kind){
 **************************/
 
 GRAVITY.AJAX_JSON_Req = function( url ){
-    var AJAX_req = new XMLHttpRequest();
-    AJAX_req.open( "GET", url, true );
-    AJAX_req.setRequestHeader( "Content-type", "application/json" );
-    AJAX_req.onreadystatechange = function(){
-        if( AJAX_req.readyState == 4 && AJAX_req.status == 200 ){
-            var contentData = JSON.parse( AJAX_req.responseText );
-            
+  var AJAX_req = new XMLHttpRequest();
+  AJAX_req.open( "GET", url, true );
+  AJAX_req.setRequestHeader( "Content-type", "application/json" );
+  AJAX_req.onreadystatechange = function(){
+    if( AJAX_req.readyState == 4 && AJAX_req.status == 200 ){
+      var contentData = JSON.parse( AJAX_req.responseText );
+
             //Add video sources from the json data
             var videoLinks = contentData[0].videoURL[0];
             var videoPlayer = document.getElementById( "videoPlayer" );
@@ -55,34 +55,26 @@ GRAVITY.AJAX_JSON_Req = function( url ){
             document.getElementById( "backImage" ).setAttribute('style', backImageURLAttr);
             
             //for defining button and other funtionality 
-            GRAVITY.initInteractivity();
+            GRAVITY.videoInit();
+          }
         }
-    }
-    AJAX_req.send();
-}
+        AJAX_req.send();
+      }
 
 /*************************
 * Hide Video controls after timeout; show them upon mouse movement.
-* Skip video button and listen audio funtionality.
+* Skip video button funtionality.
 *
 **************************/
 
-GRAVITY.initInteractivity  = function(){
-  var backVideo = document.querySelector("#backVideo video");
-  var skipButton = document.getElementById("skipButton");
-  var videoControls = document.getElementById("videoControls");
-  var backImage = document.getElementById("backImage");
-  var backImageParent = backImage.parentNode;
-  var audioPlayer = document.getElementById("audioPlayer");
-  var audioPlayStopButton = document.getElementById("audioPlayStopButton");
-  var content = document.querySelector(".content");
-  
-  var timeInterval = 3; //Time interval for in sec
-  var timeMoved = timeInterval * 10;
-
+GRAVITY.videoInit  = function(){
   //
   // Hide the video controls after timeOut. Show upon mouse movement. 
   //
+  var videoControls = document.getElementById("videoControls");
+  var timeInterval = 3; //Time interval for in sec
+  var timeMoved = timeInterval * 10;
+
   setInterval( function(){
     timeMoved++;
     if(timeMoved > (timeInterval * 10)){
@@ -99,20 +91,29 @@ GRAVITY.initInteractivity  = function(){
   }, false);
 
   //
-  // Video funtionality button. 
+  // Video/Image funtionality. 
   //
+  var backVideo = document.querySelector("#backVideo video");
+  var backImage = document.getElementById("backImage");
+  var backImageParent = backImage.parentNode;
+  
   backImageParent.removeChild(backImage);
 
   function toggleToBackImage(){
     videoControls.parentNode.removeChild(videoControls);
     backVideo.parentNode.removeChild(backVideo);
     backImageParent.appendChild(backImage);
+    GRAVITY.audioInit();
+
+    document.getElementById("status").innerHTML = "";
     setTimeout(function(){
+      
       backImage.style.opacity = 1.0;
       setTimeout(function(){
-        content.style.opacity = 1.0;
+        document.querySelector(".content").style.opacity = 1.0;
       }, 5000);
     }, 0);
+
   }
 
   //take to the background image when video is ended
@@ -120,18 +121,29 @@ GRAVITY.initInteractivity  = function(){
   
   //video status. show a progress spinner when loading.
   document.getElementById("status").innerHTML = "<i class='fa fa-spinner fa-spin fa-3x'></i>";
-  backVideo.addEventListener('loadeddata', function(){
+  backVideo.addEventListener('canplay', function(){
     document.getElementById("status").innerHTML = "";
   }, false);
 
+  var skipButton = document.getElementById("skipButton");
   skipButton.innerHTML = "Skip &nbsp; <i class='fa fa-step-forward'></i>";
   skipButton.addEventListener('click',function(){
     toggleToBackImage();
   }, false);
 
-  //
-  // Audio funtionality.
-  //
+  
+}
+
+
+/*************************
+* Audio funtionality.
+*
+**************************/
+
+GRAVITY.audioInit  = function(){
+  var audioPlayer = document.getElementById("audioPlayer");
+  var audioPlayStopButton = document.getElementById("audioPlayStopButton");
+
   function audioPlay(){
     audioPlayer.play(); 
   }
@@ -139,20 +151,22 @@ GRAVITY.initInteractivity  = function(){
   function audioPause(){
     audioPlayer.pause(); 
   }
-
-  audioPlayStopButton.innerHTML = "Listen &nbsp; <i class='fa fa-headphones'></i>";
-  audioPlayStopButton.addEventListener('click',function(){
-    if (audioPlayer.paused){
+  
+  audioPlayer.addEventListener('canplay', function(){
+    audioPlayStopButton.innerHTML = "Listen &nbsp; <i class='fa fa-headphones'></i>";
+    
+    audioPlayStopButton.addEventListener('click',function(){
+      if (audioPlayer.paused){
         audioPlay();
         audioPlayStopButton.innerHTML = "Pause &nbsp; <i style='color:red' class='fa fa-pause'></i>";
       }
-    else {
+      else {
         audioPause();
         audioPlayStopButton.innerHTML = "Listen &nbsp; <i class='fa fa-headphones'></i>";
       }
+    }, false);
+    audioPlayer.addEventListener('ended', audioPause, false);
   }, false);
-
-  audioPlayer.addEventListener('ended', audioPause, false);
 }
 
 /*************************
@@ -162,8 +176,8 @@ GRAVITY.initInteractivity  = function(){
 
   //Toggle full screen when Space is pressed
   document.addEventListener("keydown", function(e) {
-  if (e.keyCode == 32) {
-    toggleFullScreen();
+    if (e.keyCode == 32) {
+      toggleFullScreen();
     }
   }, false);
 
@@ -195,10 +209,10 @@ GRAVITY.initInteractivity  = function(){
     if (!document.fullscreenElement &&    // alternative standard method
         !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
       enterFullScreen();
-    } else {
-      exitFullScreenMy();
-    }
+  } else {
+    exitFullScreenMy();
   }
+}
 
 /*************************
 * INIT - by calling the AJAX_JSON_Req(..) funtion when DOM content is done loading.
@@ -207,5 +221,4 @@ GRAVITY.initInteractivity  = function(){
 
 document.addEventListener("DOMContentLoaded", function(){
   GRAVITY.AJAX_JSON_Req(jsonDataURL);
-  toggleFullScreen();
 }, false);
